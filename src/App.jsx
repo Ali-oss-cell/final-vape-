9b37d399f5d7e11a85ad0c35d0462fd81c08386b
Ali <ali@customer.mlnnita1.pop.starlinkisp.net>
2025-12-05 19:02:10 +0300

    Hide footer on vapes page
    - Add conditional footer rendering based on current route
    - Footer is hidden on /vapes page only
    - Footer still displays on all other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import AnimatedBackground from './components/AnimatedBackground'
@@ -10,26 +10,35 @@ import Glassware from './pages/Glassware'
 import Accessories from './pages/Accessories'
 import Stores from './pages/Stores'
 
+function AppContent() {
+  const location = useLocation()
+  const showFooter = location.pathname !== '/vapes'
+
+  return (
+    <div className="app-content">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vapes" element={<Vapes />} />
+          <Route path="/e-liquids" element={<ELiquids />} />
+          <Route path="/glassware" element={<Glassware />} />
+          <Route path="/accessories" element={<Accessories />} />
+          <Route path="/stores" element={<Stores />} />
+        </Routes>
+      </main>
+      {showFooter && <Footer />}
+    </div>
+  )
+}
+
 function App() {
   return (
     <Router>
       <div className="App">
         <AnimatedBackground />
         <AgeGate />
-        <div className="app-content">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/vapes" element={<Vapes />} />
-              <Route path="/e-liquids" element={<ELiquids />} />
-              <Route path="/glassware" element={<Glassware />} />
-              <Route path="/accessories" element={<Accessories />} />
-              <Route path="/stores" element={<Stores />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <AppContent />
       </div>
     </Router>
   )
